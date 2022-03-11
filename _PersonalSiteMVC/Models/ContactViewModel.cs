799996145f3ccd9e23a170f86fcfdaad4fa168cc
Zakary Sisler <zakarysisler@outlook.com>
2022-03-10 21:15:40 -0600

    File structure rearranged

---
+++ _PersonalSiteMVC/Models/ContactViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; //Added for access for Data Annotations (Verify valid email etc)
+
+namespace PersonalSiteMVC.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+
+        //PROPERTIES
+        [Required(ErrorMessage = "*Name is required")] 
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*Valid Email is required")] 
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "*Message is required")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+
+    }
+}
